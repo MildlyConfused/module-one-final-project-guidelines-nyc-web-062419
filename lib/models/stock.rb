815ba8f5f0f93ebe9a83b939c174b018c4bddfc2
IanGrubb <yangrubb@gmail.com>
2019-07-11 13:49:25 -0400

    Added sale price method

--- lib/models/stock.rb
+++ lib/models/stock.rb
@@ -1,4 +1,11 @@
 class Stock < ActiveRecord::Base
   belongs_to :sku
   belongs_to :location
+
+  def sale_price
+
+    SkuLocation.all.find{|sl| sl.sku == self.sku && sl.location == self.location}
+
+  end
+
 end
